150d4ea7ef886d5f2bc8cfa20f4a27e4d80fac66
Yakov <yakov_dux@bk.ru>
2018-12-02 14:08:57 +0400

    Initial commit

---
+++ www/js/slider.js
@@ -0,0 +1,9 @@
+// Can also be used with $(document).ready()
+jQuery(function($) {
+    $(window).load(function () {
+        $('.flexslider').flexslider({
+            animation: "slide",
+            slideshowSpeed: 4000
+        });
+    });
+}
