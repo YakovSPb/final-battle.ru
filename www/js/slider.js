da3d46d621b231afa1b50e07f386b9eb40c25467
Yakov <yakov_dux@bk.ru>
2018-12-02 14:17:42 +0400

    Тест отправки изменений в репозиторний

--- www/js/slider.js
+++ www/js/slider.js
@@ -1,9 +1,7 @@
 // Can also be used with $(document).ready()
-jQuery(function($) {
-    $(window).load(function () {
-        $('.flexslider').flexslider({
-            animation: "slide",
-            slideshowSpeed: 4000
-        });
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        slideshowSpeed: 4000
     });
-}
+});
